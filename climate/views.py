df980363ef577891661bcf2cfd8d9de8316cf9f4
malachi3keys <bhibbler.pltw@gmail.com>
2022-09-30 15:28:51 -0400

    update api call

--- climate/views.py
+++ climate/views.py
@@ -1,68 +1,36 @@
-from django.shortcuts import redirect, render
-# import requests
-import urllib.request
+from django.shortcuts import render
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Create your views here.
-# def index(request):
-    
-#     num = 2 + 3
-#     # query = request.body.q
-#     # units = req.body.units
-#     city = 'Paris'
-#     units = 'imperial'
-#     apiKey = os.environ['API_KEY']
-#     # url = 'https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${apiKey}'
-#     url = 'https://api.openweathermap.org/data/2.5/weather'
-#     params = {'q': city, 'appid': apiKey, 'units': units}
-
-#     r = requests.get(url=url, params=params)
-#     res = r.json()
-#     description = res['weather'][0]['description']
-#     icon = res['weather'][0]['icon']
-#     temp = res['main']['temp']
-
-#     return render(request, 'climate/index.html', {
-#         'description': description,
-#         'icon': icon,
-#         'temp': temp,
-#     })
-
 def index(request):
     if request.method == 'POST':
         city = request.POST['q']
         units = request.POST['units']
-        # city = 'Paris'
-        # units = 'imperial'
         apiKey = os.environ['API_KEY']
         
-        # source contain JSON data from API
+        # pull JSON data from API
         url = 'https://api.openweathermap.org/data/2.5/weather'
         params = {'q': city, 'appid': apiKey, 'units': units}
-        source = request.get(url=url, params=params)
+        source = requests.get(url=url, params=params)
         
-        # source = urllib.request.urlopen(
-        #     'http://api.openweathermap.org/data/2.5/weather?q=' 
-        #             + city + '&units=' + units + '&appid=' + apiKey).read()
-  
-        # converting JSON data to a dictionary
-        # list_of_data = json.loads(source)
-        list_of_data = source.json()
+        # convert JSON data to a dictionary
+        dataList = source.json()
+        print(dataList)
 
-        # data for variable list_of_data
+        # data for variable dataList
         data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']),
-            "temp": str(list_of_data['main']['temp']) + 'k',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            "icon": str(list_of_data['weather'][0]['icon']),
+            'country_code': dataList['sys']['country'],
+            'city': dataList['name'],
+            'temp': dataList['main']['temp'],
+            'description': dataList['weather'][0]['description'],
+            'pressure': dataList['main']['pressure'],
+            'humidity': dataList['main']['humidity'],
+            'icon': dataList['weather'][0]['icon'],
         }
         print(data)
     else:
         data ={}
-    return render(request, "climate/index.html", data)   
+    return render(request, 'climate/index.html', data)

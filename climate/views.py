92c8c847b32ce3100510349f5be5f2afd84406bb
malachi3keys <bhibbler.pltw@gmail.com>
2022-10-03 17:58:19 -0400

    fix error handling & update CSS

--- climate/views.py
+++ climate/views.py
@@ -11,26 +11,45 @@ def index(request):
         units = request.POST['units']
         apiKey = os.environ['API_KEY']
         
+        if units == 'imperial':
+            unit_flag = 'F'
+        else:
+            unit_flag = 'C'
+
         # pull JSON data from API
         url = 'https://api.openweathermap.org/data/2.5/weather'
         params = {'q': city, 'appid': apiKey, 'units': units}
-        source = requests.get(url=url, params=params)
         
-        # convert JSON data to a dictionary
-        dataList = source.json()
-        print(dataList)
+        try:  
+            source = requests.get(url=url, params=params)
+        except requests.exceptions.RequestException:
+            print('oops')
+        
+        if source:
+            # convert JSON data to a dictionary
+            dataList = source.json()
+            #print(dataList)
 
-        # data for variable dataList
-        data = {
-            'country_code': dataList['sys']['country'],
-            'city': dataList['name'],
-            'temp': dataList['main']['temp'],
-            'description': dataList['weather'][0]['description'],
-            'pressure': dataList['main']['pressure'],
-            'humidity': dataList['main']['humidity'],
-            'icon': dataList['weather'][0]['icon'],
-        }
-        print(data)
+            data = {
+                'country_code': dataList['sys']['country'],
+                'city': dataList['name'],
+                'temp': round(dataList['main']['temp']), 
+                'units': unit_flag,
+                'max': round(dataList['main']['temp_max']),
+                'min': round(dataList['main']['temp_min']),
+                'description': dataList['weather'][0]['description'],
+                'pressure': dataList['main']['pressure'],
+                'humidity': dataList['main']['humidity'],
+                'icon': dataList['weather'][0]['icon'],
+                'error': False,
+            }
+        else:
+            data = {
+                'error': True
+            }
     else:
-        data ={}
+        data = {}
+
+    #print(data)
+    
     return render(request, 'climate/index.html', data)
